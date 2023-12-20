ea40964b15dc5ff0401d4b0b2b1983452c66cb24
Danshicu <promchuk.danik@gmail.com>
2023-12-20 22:13:33 +0300

    initial state

--- Lab_1/main_functions.py
+++ Lab_1/main_functions.py
@@ -1,3 +1,12 @@
+
+"""
+Лабораторная работа №4 по дисциплине "Логические основы интеллектуальных систем"
+Выполнена студентами группы №121702  Шакиным И.В., Шершенем К.А., Промчуком Д.В.
+Файл содержит пример реализации алгоритма импликации Лукасевича
+Дата: 25.09.2023
+17.12.2023 - добавлен функционал, отвечающий за вывод только новых множеств
+"""
+
 import re
 import random
 
@@ -43,13 +52,17 @@ def operation_reader():
     return array_of_operations
 
 
-def parcel_transformation(level):
-    parcel_information_dict = {}
+def premise_transformation(level):
+    premises = []
+    premise_information_dict = {}
     values_dict = {}
-    with open("./Inputs/parcel.txt", 'r') as file:
+    with open("./Inputs/premise.txt", 'r') as file:
         lines = file.readlines()
 
     for line in lines:
+        subline = line.rstrip()
+        if subline.split('=')[1] not in premises:
+            premises.append(subline.split('=')[1])
         if get_spaces_count(line) != level:
             continue
         letter = line.split('=')[0]
@@ -68,10 +81,10 @@ def parcel_transformation(level):
             if variable not in array_of_variable_numbers:
                 array_of_variable_numbers.append(variable)
             array_of_variable_numbers.append(number)
-        parcel_information_dict[letter] = array_of_variable_numbers
+        premise_information_dict[letter] = array_of_variable_numbers
         values_dict[letter] = values_array
 
-    return parcel_information_dict, values_dict
+    return premise_information_dict, values_dict, premises
 
 
 def get_spaces_count(input):
@@ -114,17 +127,15 @@ def lukasiewicz_matrix_formation(input_information_dict, array_of_operations):
     return matrix_dict
 
 
-def t_norm(input_information_dict, input_values_dict, matrix_dict, parcel_information_dict, parcel_values_dict, used_rules, parcel_to_rule_list):
+def t_norm(input_information_dict, input_values_dict, matrix_dict, premise_information_dict, premise_values_dict, premise_to_rule_list):
 
     matrix_keys = list(matrix_dict.keys())
-    parcel_keys = list(parcel_information_dict.keys())
+    premise_keys = list(premise_information_dict.keys())
     #print(matrix_keys)
-    #print(parcel_keys)
+    #print(premise_keys)
     new_matrix_dict = {}
 
     for matrix_key in matrix_keys:
-        if matrix_key in used_rules:
-            continue
         pattern = re.compile(r'[A-Z]\d+|[A-Z]')
         matches = pattern.findall(matrix_key)
         first_param = matches[0]
@@ -133,50 +144,49 @@ def t_norm(input_information_dict, input_values_dict, matrix_dict, parcel_inform
         param_info = input_information_dict[first_param]
         necessary_variable = param_info[0]
 
-        necessary_parcels = []
+        necessary_premises = []
 
-        for parcel_key in parcel_keys:
-            parcel_info = parcel_information_dict[parcel_key]
-            necessary_parcel_variable = parcel_info[0]
+        for premise_key in premise_keys:
+            premise_info = premise_information_dict[premise_key]
+            necessary_premise_variable = premise_info[0]
 
-            if necessary_variable == necessary_parcel_variable:
-                if input_values_dict[first_param] == parcel_values_dict[parcel_key]:
-                    necessary_parcels.append(parcel_info[1:])
-                    parcel_to_rule_list.append(f"{parcel_key}+{matrix_key}")
+            if necessary_variable == necessary_premise_variable:
+                if input_values_dict[first_param] == premise_values_dict[premise_key]:
+                    necessary_premises.append(premise_info[1:])
+                    premise_to_rule_list.append(f"{premise_key}+{matrix_key}")
 
 
         #Пробегаемся по всем подходящим посылкам
-        for parcel in necessary_parcels:
+        for premise in necessary_premises:
             new_matrix = []
 
-            for parcel_element_id, parcel_element in enumerate(parcel):
+            for premise_element_id, premise_element in enumerate(premise):
 
-                current_row = current_matrix[parcel_element_id]
+                current_row = current_matrix[premise_element_id]
 
                 new_row = []
 
                 for matrix_element in current_row:
-                    result = max(0, parcel_element + matrix_element - 1)
+                    result = max(0, premise_element + matrix_element - 1)
                     result = round(float(result), 1)
                     new_row.append(result)
 
                 new_matrix.append(new_row)
 
-            used_rules.append(matrix_key)
             new_matrix_dict[matrix_key] = new_matrix
 
-    return new_matrix_dict, parcel_to_rule_list
+    return new_matrix_dict, premise_to_rule_list
 
 
-def strait_output(new_matrix_dict, input_information_dict, input_values_dict, parcel_information_dict, external_counter, parcel_to_rule_list):
+def strait_output(new_matrix_dict, input_information_dict, input_values_dict, premise_information_dict, external_counter, premise_to_rule_list, premises):
 
-    parcel_to_rule_counter=0
-    new_parcels_count =0
+    premise_to_rule_counter=0
+    new_premises_count =0
     alphabet = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
 
     matrix_keys = list(new_matrix_dict.keys())
-    parcel_keys = list(parcel_information_dict.keys())
-    checked_keys = list(parcel_keys)
+    premise_keys = list(premise_information_dict.keys())
+    checked_keys = list(premise_keys)
     #print(matrix_keys)
 
     #Пробегаемся по всем матрицам
@@ -195,7 +205,7 @@ def strait_output(new_matrix_dict, input_information_dict, input_values_dict, pa
             param_info = input_information_dict[second_param]
             necessary_variable = param_info[0]
 
-            letter = parcel_to_rule_list[parcel_to_rule_counter].split('+')[0]
+            letter = premise_to_rule_list[premise_to_rule_counter].split('+')[0]
             subletter = random.choice(alphabet)
             while subletter in checked_keys:
                 subletter = random.choice(alphabet)
@@ -211,35 +221,39 @@ def strait_output(new_matrix_dict, input_information_dict, input_values_dict, pa
                 array_of_elements.append(element)
 
             equation = '{' + ','.join(array_of_elements) + '}'
+            if equation not in premises:
+                premises.append(equation)
+            else:
+                continue
             result = f"{subletter}{external_counter+1}={equation}"
             line = f"\n{' '*(external_counter+1)}{result}"
             pattern = re.compile(r'[A-Z]\d+|[A-Z]')
-            matches = pattern.findall(parcel_to_rule_list[parcel_to_rule_counter].split('+')[1])
+            matches = pattern.findall(premise_to_rule_list[premise_to_rule_counter].split('+')[1])
             print(f"{letter} : {matches[0]}~>{matches[1]} |- {result}")
-            parcel_to_rule_counter+=1
-            with open('./Inputs/parcel.txt', 'a') as file:
+            premise_to_rule_counter+=1
+            with open('./Inputs/premise.txt', 'a') as file:
 
                 file.write(line)
-                new_parcels_count+=1
+                new_premises_count+=1
 
         except:
             return 0
         #finally:
-            #return new_parcels_count
-    parcel_to_rule_list.clear()
-    return new_parcels_count
+            #return new_premises_count
+    premise_to_rule_list.clear()
+    return new_premises_count
 
 
 
 
 '''
 input_information_dict = input_transformation()
-parcel_information_dict = parcel_transformation()
+premise_information_dict = premise_transformation()
 array_of_operations = operation_reader()
 
 
 
 matrix_dict = lukasiewicz_matrix_formation(input_information_dict, array_of_operations)
 print(matrix_dict)
-new_matrix_dict = t_norm(input_information_dict, matrix_dict, parcel_information_dict)
-strait_output(new_matrix_dict, input_information_dict, parcel_information_dict, 1)'''
+new_matrix_dict = t_norm(input_information_dict, matrix_dict, premise_information_dict)
+strait_output(new_matrix_dict, input_information_dict, premise_information_dict, 1)'''
